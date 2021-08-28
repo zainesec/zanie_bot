a5eedd12e5fe06bd7abe1c6d3162c6b85f4dbcb3
zaniesec <seczanie@gmail.com>
2021-08-29 02:41:19 +0900

    creating bot instance and connecting the bot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 import os
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+bot = commands.Bot(command_prefix='z>')
+
+bot.run(TOKEN)
