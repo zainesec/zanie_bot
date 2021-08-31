a0eb39c1fc1cf39d7d86a734413d1569cec38709
zaniesec <seczanie@gmail.com>
2021-08-31 22:41:42 +0900

    adding logging to the console

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import os
 
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig('logging.conf',disable_existing_loggers=False)
 logger = logging.getLogger('root')
+fileLogger = logging.getLogger('fileLogger')
 
 load_dotenv()
 
@@ -17,5 +18,6 @@ bot = commands.Bot(command_prefix='z>')
 @bot.event
 async def on_ready():
     logger.info("The bot Is UP")
+    fileLogger.info("The bot Is UP")
 
 bot.run(TOKEN)
