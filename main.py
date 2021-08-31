742f4ac11953411171b2db60162f8d8706098ba8
zaniesec <seczanie@gmail.com>
2021-08-31 21:54:15 +0900

    adding conosole handler and a file handler

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import discord
+import logging
+import logging.config
 from dotenv import load_dotenv
 from discord.ext import commands
 import os
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('root')
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix='z>')
+
+@bot.event
+async def on_ready():
+    logger.info("The bot Is UP")
 
 bot.run(TOKEN)
